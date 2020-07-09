a7ba76b72b9839246e2f76fec83143ba863b97c7
ckdesir <christopher.desir01@gmail.com>
2020-07-09 17:16:51 -0400

    Adds SessionCache class

--- src/main/webapp/scripts/sessioncache.test.js
+++ src/main/webapp/scripts/sessioncache.test.js
@@ -1,23 +1,10 @@
 import { SessionCache } from './sessioncache';
 
-test('We can check if updateController() is called +' +
-    'correct return value', () => {
+test('Checks to see if cache initializes correctly', () => {
+      try {
         const cache = new SessionCache();
-        const spy = jest.spyOn(cache, 'updateController');
-
-        expect(cache.updateController()).toBe(undefined);
-        expect(spy).toHaveBeenCalledTimes(1);
-        spy.mockRestore();
+      } catch (e) {
+        expect(e.message).toBe('Unimplemented');
       }
-);
-
-test('We can check if getValue() is called +' +
-    'correct return value', () => {
-      const cache = new SessionCache();
-      const spy = jest.spyOn(cache, 'getValue');
-
-      expect(cache.getValue('hello')).toBe('hello');
-      expect(spy).toHaveBeenCalledTimes(1);
-      spy.mockRestore();
     }
 );
