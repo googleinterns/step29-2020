63b675a8325ca254651b750a1260ce13d840e239
ckdesir <christopher.desir01@gmail.com>
2020-07-15 14:57:01 -0400

    Adds some planned unimplemented functions

--- src/main/webapp/scripts/session-script.js
+++ src/main/webapp/scripts/session-script.js
@@ -1,4 +1,53 @@
+/**
+ * Represents (in miliseconds) the cadence at which the client is refreshed. 
+ * @type {number}
+ */
+const REFRESH_CADENCE = 30000;
 
+/**
+ * This waits until the webpage loads and then it calls the
+ * anonymous function, which calls main.
+ */
+window.onload = function() { main(); }
+
+/**
+ * function main() connects the client to a session and begins many of
+ * the behind the scenes operations, like caching.
+ */
+function main() {
+  remoteToSession();
+  refresh();
+}
+
+/**
+ * function remoteToSession() uses the noVNC library
+ * in order to connect to a session.
+ */
+function remoteToSession() {
+  throw new Error('Unimplemented');
+}
+
+/**
+ * function refresh() refreshes information client side, 
+ * given how updated the server is with changes. 
+ * Checks for new attendees and for whoever the controller is.
+ */
+function refresh() {
+  updateController();
+  updateSessionInfoAttendees();
+  setTimeout(() => {
+    refresh();
+  }, REFRESH_RATE);
+}
+
+/**
+ * function updateSessionInfoAttendees() adds new attendees to the
+ * session to the session info attendee div. Also removes attendees 
+ * if they left the session. Alerts users of anyone who has left/entered.
+ */
+function updateSessionInfoAttendees() {
+  throw new Error('Unimplemented');
+}
 
 /**
  * function buildAttendeeDiv() adds the div element containing
@@ -46,6 +95,25 @@ function removeAttendeeDiv(nameOfAttendee) {
 }
 
 /**
+ * function updateController() checks to see if the current user should
+ * be the controller of their party, changing session screen privilege
+ * and updating user interface.
+ */
+function updateController() {
+  throw new Error('Unimplemented');
+}
+
+/**
+ * function changeController() updates the server with information
+ * about a new possible controller. If the current controller of the 
+ * session clicks on the controller toggle, their controller status
+ * is revoked and passed on to whoever was clicked.
+ */
+function changeController() {
+  throw new Error('Unimplemented');
+}
+
+/**
  * function openSessionInfo() displays the div container
  * that has information about the session.
  */
@@ -72,8 +140,5 @@ function copyTextToClipboard() {
   document.execCommand('copy');
 }
 
-module.exports = {
-  openSessionInfo: openSessionInfo,
-  closeSessionInfo: closeSessionInfo,
-  copyTextToClipboard: copyTextToClipboard
-};
+export { openSessionInfo, closeSessionInfo, copyTextToClipboard, 
+  buildAttendeeDiv, removeAttendeeDiv, changeController };
