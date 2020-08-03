762a83774ec3297462ff3572b66629fc737e5dfc
ckdesir <christopher.desir01@gmail.com>
2020-08-03 15:17:27 -0400

    Adds GetSessionServlet

--- src/main/java/com/google/sps/servlets/GetSessionServlet.java
+++ src/main/java/com/google/sps/servlets/GetSessionServlet.java
@@ -1,6 +1,17 @@
 package com.google.sps.servlets;
 
+import com.google.sps.data.Attendee;
+import com.google.sps.data.AttendeeInterface;
+import com.google.sps.data.DatastoreClient;
+import com.google.sps.data.DatastoreClientInterface;
+import com.google.sps.data.SessionInterface;
+import com.google.gson.*;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.Date;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +23,21 @@ public class GetSessionServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    throw new Error("Unimplemented");
+    DatastoreClientInterface datastoreClient = new DatastoreClient();
+    String sessionId = 
+        URLDecoder.decode(request.getParameter("session-id"), StandardCharsets.UTF_8);
+    String name = URLDecoder.decode(request.getParameter("name"), StandardCharsets.UTF_8);
+    AttendeeInterface updatedAttendee = new Attendee(sessionId, name, new Date());
+    datastoreClient.insertOrUpdateAttendee(updatedAttendee);
+    Optional<SessionInterface> session = datastoreClient.getSession(sessionId);
+    if (session.isPresent()) {
+      List<AttendeeInterface> listOfAttendees = datastoreClient.getAttendeesInSession(sessionId);
+      Gson gson = new Gson();
+      JsonElement jsonElement = gson.toJsonTree(session.get());
+      jsonElement.getAsJsonObject().addProperty("listOfAttendees", gson.toJson(listOfAttendees));
+      String json = gson.toJson(jsonElement);
+      response.setContentType("application/json;");
+      response.getWriter().println(json);
+    }
   }
 }
