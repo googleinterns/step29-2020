b946387b7e5120e63925fe45a2d0dcbe5d2c3525
ckdesir <christopher.desir01@gmail.com>
2020-08-03 18:15:44 -0400

    Updates Session.fromObject

--- src/main/webapp/scripts/session.js
+++ src/main/webapp/scripts/session.js
@@ -1,3 +1,5 @@
+import { Attendee } from './attendee.js';
+
 /** Attendee Class */
 class Session { 
     constructor(sessionId, ipOfVM, listOfAttendees, screenNameOfController) { 
@@ -16,19 +18,19 @@ class Session {
     } 
 
     getSessionId() {
-        return this.sessionId;
+        return this.sessionId_;
     } 
 
     getIpOfVM() {
-        return this.ipOfVM;
+        return this.ipOfVM_;
     } 
 
     getListOfAttendees() { 
-       return this.listOfAttendees;    
+       return this.listOfAttendees_;    
     }
     
     getScreenNameOfController() {
-       return this.screenNameOfController; 
+       return this.screenNameOfController_; 
     }
     /**
     * @param {Session} obj An Json Object must be passed so that the function 
@@ -37,9 +39,13 @@ class Session {
     static fromObject(obj) {   
         const session = new Session();
         session.sessionId_ = obj.sessionId;
-        session.ipOfVM_ = obj.ipOfVM;
-        session.listOfAttendees_ = obj.listOfAttendees;
-        session.screenNameOfController_ = obj.screenNameOfController;
+        session.ipOfVM_ = obj.ipOfVM.value;
+        const /** Array */ listOfAttendees = [];
+        obj.listOfAttendees.forEach(attendee => {
+          listOfAttendees.push(Attendee.fromObject(attendee));
+        })
+        session.listOfAttendees_ = listOfAttendees;
+        session.screenNameOfController_ = obj.screenNameOfController.value;
         return session;
     }
     
