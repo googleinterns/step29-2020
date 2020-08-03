ae4e51f46357f5533cde703e967b67960dffadec
ckdesir <christopher.desir01@gmail.com>
2020-08-03 15:19:11 -0400

    Adds ChangeControllerServlet

--- src/main/java/com/google/sps/servlets/ChangeControllerServlet.java
+++ src/main/java/com/google/sps/servlets/ChangeControllerServlet.java
@@ -1,6 +1,13 @@
 package com.google.sps.servlets;
 
+import com.google.sps.data.DatastoreClient;
+import com.google.sps.data.DatastoreClientInterface;
+import com.google.sps.data.Session;
+import com.google.sps.data.SessionInterface;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +19,18 @@ public class ChangeControllerServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    throw new Error("Unimplemented");
+    DatastoreClientInterface datastoreClient = new DatastoreClient();
+    String sessionId =
+        URLDecoder.decode(request.getParameter("session-id"), StandardCharsets.UTF_8);
+    String name = URLDecoder.decode(request.getParameter("name"), StandardCharsets.UTF_8);
+    Optional<SessionInterface> sessionInterface = datastoreClient.getSession(sessionId);
+    if(sessionInterface.isPresent()) {
+      SessionInterface updatedSession =
+          new Session(sessionId, Optional.of(name), sessionInterface.get().getIpOfVM());
+      datastoreClient.insertOrUpdateSession(updatedSession);
+      response.setStatus(HttpServletResponse.SC_OK);
+      return;
+    }
+    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
   }
 }
